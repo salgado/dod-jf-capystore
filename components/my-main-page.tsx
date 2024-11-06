5d68ab30540559c4bc57e93bb00e59309df858d9
Alex Salgado <alexsalgado1@gmail.com>
2024-11-06 16:03:25 +0000

    madruga bug

--- components/my-main-page.tsx
+++ components/my-main-page.tsx
@@ -35,7 +35,7 @@ export function MyMainPage() {
           <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-              Bem-vindos à Galo da Madrugada Store!
+              Bem-vindos à Galo da Madruga Store!
               </h1>
               <p className="max-w-[600px] text-muted-foreground md:text-xl">
               Exclusivo para a Galera do TDC Summit AI Recife 2024
